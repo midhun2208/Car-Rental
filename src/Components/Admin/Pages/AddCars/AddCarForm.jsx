ecaf1f364f63437acf689568cd4fa9ac160aa0bf
midhun2208 <midhun.sm343@gmail.com>
2024-03-08 17:31:37 +0530

    Car-Rental-Admim Used Cars

--- src/Components/Admin/Pages/AddCars/AddCarForm.jsx
+++ src/Components/Admin/Pages/AddCars/AddCarForm.jsx
@@ -7,20 +7,13 @@ import { MDBBtn } from "mdb-react-ui-kit";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { message } from "antd";
+import Swal from "sweetalert2";
 
 const AddCarForm = () => {
   const navigate = useNavigate();
   const [photo, setPhoto] = useState(null);
-
-  const HandelImgChange = (e) => {
-    const file = e.target.files[0];
-    setPhoto(file);
-    setCarDetails((prevDetails) => ({
-      ...prevDetails,
-      image: file,
-    }));
-  };
-
+  const [isAddingUsedCar, setIsAddingUsedCar] = useState(false);
+  const token = localStorage.getItem("token");
   const [carDetails, setCarDetails] = useState({
     make: "",
     model: "",
@@ -30,16 +23,38 @@ const AddCarForm = () => {
     image: "",
     amountperhr: Number,
   });
-  console.log(carDetails);
+  const [usedCarDetails, setUsedCarDetails] = useState({
+    make: "",
+    model: "",
+    type: "",
+    transmission: "",
+    year: "",
+    colour: "",
+    reg_number: "",
+    description: "",
+    amount: "",
+    image: null
+  });
 
+  const HandelImgChange = (e) => {
+    const file = e.target.files[0];
+    setPhoto(file);
+    setCarDetails((prevDetails) => ({
+      ...prevDetails,
+      image: file,
+    }));
+    setUsedCarDetails((prevDetails) => ({
+      ...prevDetails,
+      image: file,
+    }));
+  };
+
+  console.log(usedCarDetails);
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const token = localStorage.getItem("token");
-    console.log(token);
-    console.log(`Token: ${token}`);
 
     if (!token) {
-      navigate('/login')
+      navigate("/login");
       return;
     }
 
@@ -71,7 +86,7 @@ const AddCarForm = () => {
         year: "",
         colour: "",
         reg_number: "",
-        image: null, // Clear the image after upload
+        image: null,
         amountperhr: 0,
       });
       navigate("/adminDashboard");
@@ -81,114 +96,368 @@ const AddCarForm = () => {
     }
   };
 
+  const handleAddUsedCars = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("make", usedCarDetails.make);
+    formData.append("model", usedCarDetails.model);
+    formData.append("type", usedCarDetails.type);
+    formData.append("transmission", usedCarDetails.transmission);
+    formData.append("year", usedCarDetails.year);
+    formData.append("colour",usedCarDetails.colour);
+    formData.append("reg_number", usedCarDetails.reg_number);
+    formData.append("description", usedCarDetails.description);
+
+    formData.append("amount", usedCarDetails.amount);
+    formData.append("image", usedCarDetails.image);
+
+    try {
+      const response = await axios.post(
+        "http://127.0.0.1:8000/adminapi/usedvehicles/",
+        formData,
+        {
+          headers: {
+            Authorization: `Token ${token}`,
+          },
+        }
+      );
+      console.log(response.data);
+      Swal.fire({
+        position: "top-center",
+        icon: "success",
+        title: "A New Used Car Added",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      setUsedCarDetails({
+      make: "",
+      model: "",
+      type: "",
+      transmission: "",
+      year: "",
+      colour: "",
+      reg_number: "",
+      description: "",
+      image: "",
+      amount: "",
+    });
+    } catch (error) {
+      console.log(error);
+      Swal.fire({
+        position: "top-center",
+        icon: "error",
+        title: "Oops....",
+        text: "Something went wrong!",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    }
+  };
+
+  const handleToggle = () => {
+    setIsAddingUsedCar(!isAddingUsedCar);
+    setCarDetails({
+      make: "",
+      model: "",
+      year: "",
+      colour: "",
+      reg_number: "",
+      image: null,
+      amountperhr: 0,
+    });
+    setUsedCarDetails({
+      make: "",
+      model: "",
+      type: "",
+      transmission: "",
+      year: "",
+      colour: "",
+      reg_number: "",
+      description: "",
+      image: "",
+      amount: "",
+    });
+  };
+
   return (
     <>
       <div className="addCar-main">
         <Container>
           <Row>
             <Col lg={3}></Col>
-            <Col lg={6} md={12} className="mt-5 p-5 addcar-card">
+            <Col lg={6} md={12} className=" pt-3 px-5 mb-5 addcar-card">
               <Card className="addcar-card2">
                 <div className="add-car text-center mt-3">
-                  <h2 className="add-car-head">Add Rental Car</h2>
-                </div>
-                <form className="p-4 form-addcar" onSubmit={handleSubmit}>
-                  <TextField
-                    id=""
-                    label="Enter car make"
-                    variant="filled"
-                    fullWidth
-                    className="mt-1 text-filed"
-                    value={carDetails.make}
-                    onChange={(e) =>
-                      setCarDetails({ ...carDetails, make: e.target.value })
-                    }
-                    required
-                  />
-                  <TextField
-                    id=""
-                    label="Enter car model"
-                    variant="filled"
-                    fullWidth
-                    className="mt-3 text-filed"
-                    value={carDetails.model}
-                    onChange={(e) =>
-                      setCarDetails({ ...carDetails, model: e.target.value })
-                    }
-                    required
-                  />
-                  <TextField
-                    id=""
-                    label="Enter year"
-                    variant="filled"
-                    fullWidth
-                    className="mt-3 text-filed"
-                    type="number"
-                    value={carDetails.year}
-                    onChange={(e) =>
-                      setCarDetails({ ...carDetails, year: e.target.value })
-                    }
-                    required
-                  />
-                  <TextField
-                    id=""
-                    label="Enter colour"
-                    variant="filled"
-                    fullWidth
-                    className="mt-3 text-filed"
-                    value={carDetails.colour}
-                    onChange={(e) =>
-                      setCarDetails({ ...carDetails, colour: e.target.value })
-                    }
-                    required
-                  />
-                  <TextField
-                    id=""
-                    label="Enter registration number"
-                    variant="filled"
-                    fullWidth
-                    className="mt-3 text-filed"
-                    value={carDetails.reg_number}
-                    onChange={(e) =>
-                      setCarDetails({
-                        ...carDetails,
-                        reg_number: e.target.value,
-                      })
-                    }
-                    required
-                  />
-                  <TextField
-                    id=""
-                    label="Enter amount per hour"
-                    variant="filled"
-                    fullWidth
-                    className="mt-3 text-filed"
-                    type="number"
-                    value={carDetails.amountperhr}
-                    onChange={(e) =>
-                      setCarDetails({
-                        ...carDetails,
-                        amountperhr: e.target.value,
-                      })
-                    }
-                    required
-                  />
-                  <TextField
-                    id=""
-                    variant="filled"
-                    fullWidth
-                    className="mt-3 text-filed"
-                    type="file"
-                    onChange={HandelImgChange}
-                    required
-                  />
-
-                  <div className="p-3">
-                    <MDBBtn type="submit" className="btn btn-primary " block>
-                      Add Car
+                  <h2 className="add-car-head">
+                    {isAddingUsedCar ? "Add Used Car" : "Add Rental Car"}
+                  </h2>
+                  <div className="px-3 mb-2">
+                    <MDBBtn onClick={handleToggle} className="btn btn-parimary">
+                      {isAddingUsedCar
+                        ? "Switch to Add Rental Car"
+                        : "Switch to Add Used Car"}
                     </MDBBtn>
                   </div>
-                </form>
+                </div>
+                {isAddingUsedCar ? (
+                  <form
+                    className="p-4 form-addcar"
+                    onSubmit={handleAddUsedCars}
+                  >
+                    <TextField
+                      id=""
+                      label="Enter car make"
+                      variant="filled"
+                      fullWidth
+                      className="mt-1 text-filed"
+                      value={usedCarDetails.make}
+                      onChange={(e) =>
+                        setUsedCarDetails({
+                          ...usedCarDetails,
+                          make: e.target.value,
+                        })
+                      }
+                      required
+                    />
+                    <TextField
+                      id=""
+                      label="Enter car model"
+                      variant="filled"
+                      fullWidth
+                      className="mt-3 text-filed"
+                      value={usedCarDetails.model}
+                      onChange={(e) =>
+                        setUsedCarDetails({
+                          ...usedCarDetails,
+                          model: e.target.value,
+                        })
+                      }
+                      required
+                    />
+                    <TextField
+                      id=""
+                      label="Enter year"
+                      variant="filled"
+                      fullWidth
+                      className="mt-3 text-filed"
+                      type="number"
+                      value={usedCarDetails.year}
+                      onChange={(e) =>
+                        setUsedCarDetails({
+                          ...usedCarDetails,
+                          year: e.target.value,
+                        })
+                      }
+                      required
+                    />
+                    <TextField
+                      id=""
+                      label="Enter colour"
+                      variant="filled"
+                      fullWidth
+                      className="mt-3 text-filed"
+                      value={usedCarDetails.colour}
+                      onChange={(e) =>
+                        setUsedCarDetails({
+                          ...usedCarDetails,
+                          colour: e.target.value,
+                        })
+                      }
+                      required
+                    />
+                    <TextField
+                      id=""
+                      label="Enter Type"
+                      variant="filled"
+                      fullWidth
+                      className="mt-3 text-filed"
+                      value={usedCarDetails.type}
+                      onChange={(e) =>
+                        setUsedCarDetails({
+                          ...usedCarDetails,
+                          type: e.target.value,
+                        })
+                      }
+                      required
+                    />
+                    <TextField
+                      id=""
+                      label="Enter transmission"
+                      variant="filled"
+                      fullWidth
+                      className="mt-3 text-filed"
+                      value={usedCarDetails.transmission}
+                      onChange={(e) =>
+                        setUsedCarDetails({
+                          ...usedCarDetails,
+                          transmission: e.target.value,
+                        })
+                      }
+                      required
+                    />
+                    <TextField
+                      id=""
+                      label="Enter registration number"
+                      variant="filled"
+                      fullWidth
+                      className="mt-3 text-filed"
+                      value={usedCarDetails.reg_number}
+                      onChange={(e) =>
+                        setUsedCarDetails({
+                          ...usedCarDetails,
+                          reg_number: e.target.value,
+                        })
+                      }
+                      required
+                    />
+                    <TextField
+                      id=""
+                      label="Enter description"
+                      variant="filled"
+                      fullWidth
+                      className="mt-3 text-filed"
+                      multiline
+                      value={usedCarDetails.description}
+                      onChange={(e) =>
+                        setUsedCarDetails({
+                          ...usedCarDetails,
+                          description: e.target.value,
+                        })
+                      }
+                      required
+                    />
+                    <TextField
+                      id=""
+                      label="Enter amount"
+                      variant="filled"
+                      fullWidth
+                      className="mt-3 text-filed"
+                      type="number"
+                      value={usedCarDetails.amount}
+                      onChange={(e) =>
+                        setUsedCarDetails({
+                          ...usedCarDetails,
+                          amount: e.target.value,
+                        })
+                      }
+                      required
+                    />
+                    <TextField
+                      id=""
+                      variant="filled"
+                      fullWidth
+                      className="mt-3 text-filed"
+                      type="file"
+                      onChange={HandelImgChange}
+                      required
+                    />
+
+                    <div className="p-3">
+                      <MDBBtn type="submit" className="btn btn-primary" block>
+                        Add Used Car
+                      </MDBBtn>
+                    </div>
+                  </form>
+                ) : (
+                  //Form for the rental car adding
+                  <form className="p-4 form-addcar" onSubmit={handleSubmit}>
+                    <TextField
+                      id=""
+                      label="Enter car make"
+                      variant="filled"
+                      fullWidth
+                      className="mt-1 text-filed"
+                      value={carDetails.make}
+                      onChange={(e) =>
+                        setCarDetails({ ...carDetails, make: e.target.value })
+                      }
+                      required
+                    />
+                    <TextField
+                      id=""
+                      label="Enter car model"
+                      variant="filled"
+                      fullWidth
+                      className="mt-3 text-filed"
+                      value={carDetails.model}
+                      onChange={(e) =>
+                        setCarDetails({ ...carDetails, model: e.target.value })
+                      }
+                      required
+                    />
+                    <TextField
+                      id=""
+                      label="Enter year"
+                      variant="filled"
+                      fullWidth
+                      className="mt-3 text-filed"
+                      type="number"
+                      value={carDetails.year}
+                      onChange={(e) =>
+                        setCarDetails({ ...carDetails, year: e.target.value })
+                      }
+                      required
+                    />
+                    <TextField
+                      id=""
+                      label="Enter colour"
+                      variant="filled"
+                      fullWidth
+                      className="mt-3 text-filed"
+                      value={carDetails.colour}
+                      onChange={(e) =>
+                        setCarDetails({ ...carDetails, colour: e.target.value })
+                      }
+                      required
+                    />
+                    <TextField
+                      id=""
+                      label="Enter registration number"
+                      variant="filled"
+                      fullWidth
+                      className="mt-3 text-filed"
+                      value={carDetails.reg_number}
+                      onChange={(e) =>
+                        setCarDetails({
+                          ...carDetails,
+                          reg_number: e.target.value,
+                        })
+                      }
+                      required
+                    />
+                    <TextField
+                      id=""
+                      label="Enter amount per hour"
+                      variant="filled"
+                      fullWidth
+                      className="mt-3 text-filed"
+                      type="number"
+                      value={carDetails.amountperhr}
+                      onChange={(e) =>
+                        setCarDetails({
+                          ...carDetails,
+                          amountperhr: e.target.value,
+                        })
+                      }
+                      required
+                    />
+                    <TextField
+                      id=""
+                      variant="filled"
+                      fullWidth
+                      className="mt-3 text-filed"
+                      type="file"
+                      onChange={HandelImgChange}
+                      required
+                    />
+
+                    <div className="mt-4">
+                      <MDBBtn type="submit" className="btn btn-primary" block>
+                        Add Rental Car
+                      </MDBBtn>
+                    </div>
+                  </form>
+                )}
               </Card>
             </Col>
             <Col lg={3}></Col>
--- src/Components/Admin/Pages/CarRentalCards/CarCards.jsx
+++ src/Components/Admin/Pages/CarRentalCards/CarCards.jsx
@@ -1,21 +1,24 @@
 import React, { useEffect, useState } from "react";
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import { MDBTable, MDBTableHead, MDBTableBody } from "mdb-react-ui-kit";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
+import "./CarCards.css";
 
 function CarCards() {
   const [carsData, setCarsData] = useState(null);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const [usedCarsData, setUsedCarsData] = useState(null)
+  const token = localStorage.getItem("token");
 
   useEffect(() => {
     getCarsDetails();
+    getUsedCarDetails();
   }, []);
 
   const getCarsDetails = async () => {
-    const token = localStorage.getItem("token");
     if (!token) {
-      navigate('/login')
+      navigate("/login");
     } else {
       try {
         const response = await axios.get(
@@ -33,45 +36,102 @@ function CarCards() {
     }
   };
 
-  if (carsData === null)
+  const getUsedCarDetails = async()=>{
+    try {
+      const response = await axios.get("http://127.0.0.1:8000/adminapi/usedvehicles/",{
+        headers:{
+          Authorization: `Token ${token}`
+        }
+      })
+      console.log(response.data.vehicles);
+      setUsedCarsData(response.data.vehicles)
+    } catch (error) {
+      
+    }
+  }
+
+  if (carsData === null ||usedCarsData === null)
     return (
       <div>
         <h1 className="text-center p-5">Loading....</h1>
       </div>
     );
-  console.log(carsData);
 
   return (
     <>
-      <Container>
-        <div>
-          <h2 className="text-center my-5">All Rental-Cars</h2>
-        </div>
-        <MDBTable hover>
-          <MDBTableHead>
-            <tr>
-              <th scope="col">S.NO</th>
-              <th scope="col">Car Make</th>
-              <th scope="col">Car Model</th>
-              <th scope="col">Car Registration</th>
-              <th scope="col">Car Rental Status</th>
-              <th scope="col">Action</th>
-            </tr>
-          </MDBTableHead>
-          <MDBTableBody>
-            {carsData.map((item, index) => (
-              <tr key={item.id}>
-                <th scope="row">{index + 1}</th>
-                <td>{item.make}</td>
-                <td>{item.model}</td>
-                <td>{item.reg_number}</td>
-                <td>{item.rental_status}</td>
-                <td><Link to={"/adminDashboard/viewCar/"+item.id}><button className="btn btn-primary"><i class="fa-solid fa-file-pen"></i></button></Link></td>
-                
-              </tr>
-            ))}
-          </MDBTableBody>
-        </MDBTable>
+      <Container fluid>
+        <Row className="p-5">
+          <Col lg={6} className="px-2">
+            <div>
+              <h2 className="text-center mb-5 ">All Rental-Cars</h2>
+            </div>
+
+            <div className="allrental-cars shadow-lg rounded-lg">
+              <MDBTable hover>
+                <MDBTableHead>
+                  <tr>
+                    <th scope="col">S.NO</th>
+                    <th scope="col">Make</th>
+                    <th scope="col">Model</th>
+                    <th scope="col">Registration_NO</th>
+                    <th scope="col">Rental Status</th>
+                    <th scope="col">Action</th>
+                  </tr>
+                </MDBTableHead>
+                <MDBTableBody>
+                  {carsData.map((item, index) => (
+                    <tr key={item.id}>
+                      <th scope="row">{index + 1}</th>
+                      <td>{item.make}</td>
+                      <td>{item.model}</td>
+                      <td>{item.reg_number}</td>
+                      <td>{item.rental_status}</td>
+                      <td>
+                        <Link to={"/adminDashboard/viewCar/" + item.id}>
+                          <button className="btn btn-primary">
+                            <i class="fa-solid fa-file-pen"></i>
+                          </button>
+                        </Link>
+                      </td>
+                    </tr>
+                  ))}
+                </MDBTableBody>
+              </MDBTable>
+            </div>
+          </Col>
+          <Col lg={6} className="px-4">
+            <div>
+              <h2 className="text-center mb-5 ">All Used-Cars</h2>
+            </div>
+
+            <div className="allused-cars shadow-lg rounded-lg">
+              <MDBTable hover>
+                <MDBTableHead>
+                  <tr>
+                    <th scope="col">S.NO</th>
+                    <th scope="col">Model</th>
+                    <th scope="col">Type</th>
+                    <th scope="col">Registration_NO</th>
+                    <th scope="col">Rate</th>
+                    <th scope="col">Year</th>
+                  </tr>
+                </MDBTableHead>
+                <MDBTableBody>
+                  {usedCarsData.map((item, index) => (
+                    <tr key={item.id}>
+                      <th scope="row">{index + 1}</th>
+                      <td>{item.model}</td>
+                      <td>{item.type}</td>
+                      <td>{item.reg_number}</td>
+                      <td>{item.amount} &#8377;</td>
+                      <td>{item.year} </td>
+                    </tr>
+                  ))}
+                </MDBTableBody>
+              </MDBTable>
+            </div>
+          </Col>
+        </Row>
       </Container>
     </>
   );
